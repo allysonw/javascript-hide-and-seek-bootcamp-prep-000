da72b847c58f98b6815972c607de3444ffab7d78
Allyson Wesman <allysonwesman@gmail.com>
2017-12-26 23:05:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,4 +13,9 @@ function deepestChild() {
 }
 
 function increaseRankBy(n) {
+  var list = document.querySelectorAll(".ranked-list li");
+  for (var i = 0; i < list.length; i++)
+  {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+  } 
 }
