e8194bb676fe451e69f6314eaa3847f1cace14b6
Allyson Wesman <allysonwesman@gmail.com>
2017-12-26 22:57:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,12 +8,19 @@ function nestedTarget() {
 
 function deepestChild() {
   var list = document.getElementById('grand-node').querySelectorAll("div");
-  console.log("list");
-  while (list.[i] != 1)
+  console.log("Before loop, list is : " + list + " and it's typeof: " + typeof list);
+  var i = 0;
+  while (list[i])
   {
-    list = document.getElementById('grand-node').querySelectorAll("div");
+    console.log("List[i] in loop is: " + list[i]);
+    console.log("List[i] inner HTML in loop is: " + list[i].innerHTML);
+    i++;
+    //list = document.getElementById(list[i]);
+    console.log("After incrementing, List[i] in loop is: " + list[i]);
   }
-  return list.[i];
+
+  console.log("List[i] after loop is: " + list[i]);
+  return list[i-1].innerHTML;
   //for (let i = 0; i < list.length; i++) {
     //  if
   //}
